e2d288898d01f547b16abc0a46036414c4658892
1301329-NB\wistronits <peterhu0929@gmail.com>
2020-12-02 18:29:09 +0800

    add Angular Social Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { GoogleLoginProvider, SocialAuthService, SocialUser } from 'angularx-social-login';
+import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Frostyfox';
+  user: SocialUser;
+  loggedIn: boolean;
+  constructor(private authService: SocialAuthService) { }
+
+  ngOnInit() {
+    this.getUserInfo();
+  }
+
+
+  getUserInfo() {
+    this.authService.authState.subscribe(x => {
+      this.user = x;
+      this.loggedIn = (x != null);
+      console.log(this.user);
+    });
+  }
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+  signOut(): void {
+    this.authService.signOut();
+  }
+  refreshToken(): void {
+    const refresh$ = of(this.authService.refreshAuthToken(GoogleLoginProvider.PROVIDER_ID));
+    refresh$.subscribe(x => this.getUserInfo());
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,11 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';// <-- NgModel
 import { ProductComponent } from './programs/product/product.component';
 import { ShareDialogComponent } from './share/share-dialog/share-dialog.component';
 import { ProductEditComponent } from './programs/product-edit/product-edit.component';
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider
+} from 'angularx-social-login';
 
 @NgModule({
   declarations: [
@@ -26,9 +31,27 @@ import { ProductEditComponent } from './programs/product-edit/product-edit.compo
     AngularMaterialModule,
     PipeModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: false,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider(
+            '878583083538-ihdvqifjlp4jvt5887itldsojas1u72m.apps.googleusercontent.com'
+          )
+        },
+        {
+          id: FacebookLoginProvider.PROVIDER_ID,
+          provider: new FacebookLoginProvider('clientId')
+        }
+      ]
+    } as SocialAuthServiceConfig,
+  }],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
