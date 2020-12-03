0ad7a65ed8bb7931cf1580d6a448fbd041e9e082
1301329-NB\wistronits <peterhu0929@gmail.com>
2020-12-03 17:36:17 +0800

    add userprofile & IdentityClaims display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { OAuthModule, OAuthService, AuthConfig } from 'angular-oauth2-oidc';
 import { JwksValidationHandler } from 'angular-oauth2-oidc-jwks';
+import { of } from 'rxjs/internal/observable/of';
 import { authCodeFlowConfig } from './sso.config';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   title = 'Frostyfox';
   isAuthenticated;
 
   AccessToken;
   userProfile;
+  IdentityClaims;
+
   constructor(private oAuthService: OAuthService) {
     this.configureSingleSignOn();
   }
-
+  ngOnInit() {
+    this.oAuthService.events
+      .pipe(filter(e => e.type === 'token_received'))
+      .subscribe(
+        x => { this.getUserProfile(); this.getToken(); this.getIdentityClaims(); }
+      );
+  }
   configureSingleSignOn() {
     this.oAuthService.configure(authCodeFlowConfig);
     this.oAuthService.tokenValidationHandler = new JwksValidationHandler();
     this.oAuthService.loadDiscoveryDocumentAndTryLogin();
+    // this.oAuthService.events
+    //   .pipe(filter(e => e.type === 'token_received'))
+    //   .subscribe(
+    //     x => { this.Test(); console.log(x); }
+    //   );
   }
   login() {
     this.oAuthService.initImplicitFlow();
+    // login$.subscribe(x => this.Test());
   }
   logout() {
     this.oAuthService.logOut();
   }
   getToken() {
-    let AccessToken = this.oAuthService.getAccessToken();
-    console.log(AccessToken);
+    this.AccessToken = this.oAuthService.getAccessToken();
+    console.log(this.AccessToken);
   }
-  Test() {
+  getUserProfile() {
     this.oAuthService.loadUserProfile().then(
       x => {
-        this.userProfile = x,
-          console.log(this.userProfile.email);
+        this.userProfile = JSON.parse(JSON.stringify(x)),
+          console.log(this.userProfile);
       }
     );
   }
+  getIdentityClaims() {
+    this.IdentityClaims = this.oAuthService.getIdentityClaims();
+    console.log(this.IdentityClaims);
+  }
+
   get Token() {
     let claims: any = this.oAuthService.getIdentityClaims();
     // console.log(claims);
